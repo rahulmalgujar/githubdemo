5be8d6be63ce2214ddc655a248c1ac5ff3c98a48
rahultl <tl@gmail.com>
2017-04-30 19:15:40 +0530

    hii..DEV 1 commiting 2nd time...

--- TLGitProj1/src/com/nt/git/HelloWorldTest.java
+++ TLGitProj1/src/com/nt/git/HelloWorldTest.java
@@ -5,5 +5,6 @@ public class HelloWorldTest {
 	public static void main(String[] args) {
 		System.out.println("Hello...My name is Rahul..");
 		System.out.println("Hello TL...I am Dev2..");
+		System.out.println("Hello TL...I am Dev1..");
 	}
 }
